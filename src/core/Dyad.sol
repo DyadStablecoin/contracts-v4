79079492e1646b53860249de4caadbfd59e66595
shafu.eth <selfouly@gmail.com>
2023-03-19 16:06:44 +0100

    rm out

--- src/core/Dyad.sol
+++ src/core/Dyad.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20Upgradeable} from "@openzeppelin-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
 import {Initializable} from "@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol";
 
