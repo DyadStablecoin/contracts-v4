0937e233f97ce8211d2187b565f11da09b43d164
shafu.eth <selfouly@gmail.com>
2023-03-22 14:38:27 +0100

    factory tests

--- test/VaultFactory.t.sol
+++ test/VaultFactory.t.sol
@@ -6,6 +6,26 @@ import {BaseTest} from "./BaseTest.sol";
 contract VaultFactoryTest is BaseTest {
   function test_correctDeployment() public {
     assertTrue(factory.vaults(MAINNET_WETH, MAINNET_ORACLE) != address(0));
+    assertTrue(dNft.isLiquidator(address(vault)));
+    assertTrue(address(vault.dNft())   != address(0));
+    assertTrue(address(vault.collat()) != address(0));
+    assertTrue(address(vault.oracle()) != address(0));
+
+    assertTrue(bytes(vault.dyad().symbol()).length > 0);
+    assertTrue(bytes(vault.dyad().name())  .length > 0);
+    assertTrue(vault.dyad().owner()    == address(vault));
+  }
+  function test_fail_deployWithWrongOracle() public {
+    vm.expectRevert();
+    factory.deploy(MAINNET_WETH, address(0));
+  }
+  function test_fail_deployWithWrongCollateral() public {
+    vm.expectRevert();
+    factory.deploy(address(0), MAINNET_ORACLE);
+  }
+  function test_fail_deployWithSameOracleAndCollateral() public {
+    vm.expectRevert();
+    factory.deploy(MAINNET_WETH, MAINNET_WETH);
   }
   function test_fail_deploySameVaultAgain() public {
     vm.expectRevert();
